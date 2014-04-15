9610868fd5544c0898a7055b5e1e9e9be5018e64
Ward Fisher <wfisher@unidata.ucar.edu>
2014-04-15 17:14:30 -0600

    Got the first version of the dashboard working properly.

---
+++ CI.cmake
@@ -0,0 +1,71 @@
+#####
+# Some Test/System Setup
+#####
+set (CTEST_PROJECT_NAME "netcdf-c")
+
+
+# Get Hostname
+find_program(HOSTNAME_CMD NAMES hostname)
+exec_program(${HOSTNAME_CMD} OUTPUT_VARIABLE HOSTNAME)
+set(CTEST_SITE "${HOSTNAME}")
+
+# Get system configuration
+find_program(UNAME NAMES uname)
+macro(getuname name flag)
+	exec_program("${UNAME}" ARGS "${flag}" OUTPUT_VARIABLE "${name}")
+endmacro(getuname)
+getuname(osname -s)
+getuname(osrel  -r)
+getuname(cpu    -m)
+set(CTEST_BUILD_NAME        "${osname}-${osrel}-${cpu}")
+
+# Set locations of src/build
+set (CTEST_DASHBOARD_ROOT "${CTEST_SCRIPT_DIRECTORY}/Dashboards")
+SET (CTEST_SOURCE_DIRECTORY "${CTEST_DASHBOARD_ROOT}/netcdf-src")
+SET (CTEST_BINARY_DIRECTORY "${CTEST_DASHBOARD_ROOT}/builds/build-cont")
+set(ENV{LC_ALL} C)
+
+####
+# End Test/System Setup
+#####
+
+set (CTEST_CMAKE_GENERATOR "Unix Makefiles")
+FIND_PROGRAM(GITNAMES NAMES git)
+
+set (CTEST_GIT_COMMAND ${GITNAMES})
+set (CTEST_COMMAND "\"${CTEST_EXECUTABLE_NAME}\" -D Continuous")
+
+set (CTEST_CHECKOUT_COMMAND "${CTEST_GIT_COMMAND} clone file:///vagrant/netcdf-c ${CTEST_SOURCE_DIRECTORY}")
+set (CTEST_UPDATE_COMMAND ${CTEST_GIT_COMMAND})
+set (CTEST_START_WITH_EMPTY_BINARY_DIRECTORY TRUE)
+
+## Set CTest Options
+set(OPTIONS -DENABLE_EXTRA_TESTS=ON -DENABLE_HDF4=ON)
+
+## Kick off the test
+SET (CTEST_START_WITH_EMPTY_BINARY_DIRECTORY_ONCE 1)
+#ctest_start("Continuous")
+set (first_loop 1)
+ctest_start("Continuous")
+
+while (${CTEST_ELAPSED_TIME} GREATER -1)
+	set (START_TIME ${CTEST_ELAPSED_TIME})
+	ctest_update(RETURN_VALUE count)
+	message("Count: ${count}")
+	if (count GREATER 0 OR first_loop GREATER 0)
+		SET(CTEST_BUILD_NAME	"${CTEST_BUILD_NAME}-1")
+		
+		message("Count ${count} > 0, running analysis.")
+		ctest_configure(OPTIONS "${OPTIONS}")
+		message("Configuring")
+		ctest_build()
+		message("Building")
+		ctest_test()
+		message("Testing")
+		ctest_submit()
+		message("Submitting")
+		message("Analysis complete.")
+		set(first_loop 0)
+	endif()
+	ctest_sleep( ${START_TIME} 60 ${CTEST_ELAPSED_TIME})
+endwhile()
--- bootstrap_cdash.sh
+++ bootstrap_cdash.sh
@@ -5,55 +5,68 @@
 apt-get update
 
 export DEBIAN_FRONTEND=noninteractive
-apt-get -y -q install wget apache2 mysql-server php5 php5-mysql php5-xsl php5-curl php5-gd unzip subversion
+apt-get -y -q install wget apache2 mysql-server php5 php5-mysql php5-xsl php5-curl php5-gd unzip subversion links
 
 ##### 
 # Install CDash, configure mysql
 #####
 
-sudo -i -u vagrant svn co https://www.kitware.com/svn/CDash/Release-2-0-2 CDash
+cd /var/www/
 
-mysql -u root -e "grant all on *.* to 'cdash'@'localhost'"
+echo '<?php' > /var/www/info.php
+echo 'phpinfo();' >> /var/www/info.php
+echo '?>' >> /var/www/info.php
 
 
+###
+# Check out and configure CDash, if need be.
+###
+if [ ! -f /var/www/CDash ]; then
+    # Stop mysql for the time being.
+    stop mysql
+
+    svn co https://www.kitware.com/svn/CDash/Release-2-0-2 CDash
+    # Start mysql back up.
+    start mysql
+
+    # Configure MySQL
+    # Add cdash database, user.
+    mysql -u root -e "create user 'cdash'@'%'"
+    mysql -u root -e "grant all on *.* to 'cdash'@'%'"
+    mysql -u root -e "create database cdash"
+
+
+    # If there is a default database file,
+    # import it into the database.
+    # Otherwise, create a script that lives in /home/vagrant
+    # that can be used to export the database file.
+    if [ -f /vagrant/default_cdash_database.sql.gz ]; then
+	echo "Importing pre-existing database."
+	gunzip < /vagrant/default_cdash_database.sql.gz | mysql -u root cdash 
+    else
+	# Create a script for easy export of database, once it's been configured.
+	echo "You must create a default database for use with CDash."
+	echo "Once created, run the script in /home/vagrant to"
+	echo "export it."
+	echo ""
+    fi
+    SQLSCRIPT="/home/vagrant/mysql_export.sh"
+    echo '#!/bin/bash' > $SQLSCRIPT
+    echo 'set -x' >> $SQLSCRIPT
+    echo "mysqldump -u root cdash | gzip > /home/vagrant/default_cdash_database.sql.gz" >> $SQLSCRIPT
+    echo "mv /home/vagrant/default_cdash_database.sql.gz /vagrant" >> $SQLSCRIPT
+    echo "echo Finished." >> $SQLSCRIPT
+    chmod 755 $SQLSCRIPT
+    chown vagrant:vagrant $SQLSCRIPT
+    
+    apache2ctl restart
+fi
+
+# Clone a base netcdf-c directory to work from and to monitor for changes.
+clone git://github.com/Unidata/netcdf-c /vagrant/netcdf-c
 
 exit 0
 
-#####
-# Configure mysql. It will live in /vagrant/mysql.
-# This must be created, if need be. 
-#####
 
 
 
-###
-# Move data directory.
-###
-
-stop mysql
-
-# Create the target data directory.
-sudo -i -u vagrant mkdir -p /vagrant/mysql
-
-# Change the setting in /etc/mysql/my.cnf
-cp /etc/mysql/my.cnf /etc/mysql/my.cnf.orig
-sed -i 's/datadir\s\{0,\}= \/var\/lib\/mysql/datadir = \/vagrant\/mysql/g' /etc/mysql/my.cnf
-cp /etc/mysql/my.cnf /etc/mysql/my.cnf.new
-
-# Update apparmor, insert new directory 
-AACFG="/etc/apparmor.d/usr.sbin.mysqld"
-sed -i '/\/var\/lib\/mysql\/ r,/a \/vagrant\/mysql\/ r,' $AACFG
-sed -i '/\/var\/lib\/mysql\/\*\* rwk,/a \/vagrant\/mysql\/\*\* rwk,' $AACFG
-
-find /var/lib/mysql -maxdepth 1 -type d -exec cp -R {} /vagrant/mysql \;
-
-sudo apparmor_parser -r $AACFG
-
-# Restart mysql
-#stop mysql
-#start mysql
-
-
-###
-# End move data directory.
-###
