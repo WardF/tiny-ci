d5335ec683098ef8411d61c0b3c51dbf290b638b
Ward Fisher <wfisher@unidata.ucar.edu>
2014-06-06 14:29:27 -0600

    Refined CI script, bootstrap script.

--- bootstrap_ci.sh
+++ bootstrap_ci.sh
@@ -6,23 +6,31 @@ USE_APT=""
 USE_YUM=""
 #### Install base development packages.
 if [ `which apt-get | wc -w` -gt 0 ]; then
-USE_APT="TRUE"
-PKG_CMD=`which apt-get`
-PKG_UPDATE="$PKG_CMD update"
-PKG_LIST="ubuntu-dev-tools m4 git libjpeg-dev libcurl4-openssl-dev wget htop libtool bison flex autoconf curl g++ midori"
-GRP_LIST=""
+    USE_APT="TRUE"
+    PKG_CMD=`which apt-get`
+    PKG_UPDATE="$PKG_CMD update"
+    PKG_LIST="ubuntu-dev-tools m4 git libjpeg-dev libcurl4-openssl-dev wget htop libtool bison flex autoconf curl g++ midori"
+    GRP_LIST=""
 elif [ `which yum | wc -w` -gt 0 ]; then
-USE_YUM="TRUE"
-PKG_UPDATE=""
-PKG_CMD=`which yum`
-PKG_LIST="m4 git libjpeg-turbo-devel libcurl-devel wget nano libtool bison flex autoconf curl"
-GRP_LIST="yum -y groupinstall 'Development tools'"
+    USE_YUM="TRUE"
+    PKG_UPDATE=""
+    PKG_CMD=`which yum`
+    PKG_LIST="m4 git libjpeg-turbo-devel libcurl-devel wget nano libtool bison flex autoconf curl"
+    GRP_LIST="yum -y groupinstall 'Development tools'"
 fi
 
 $PKG_UPDATE
 $PKG_CMD -y upgrade
 $PKG_CMD -y install $PKG_LIST
 $GRP_LIST
+#####
+# Enable Cron to run automatically.
+#####
+if [ `which update-rc.d | wc -w` -gt 0 ]; then
+    update-rc.d crond defaults
+elif [ `which chkconfig | wc -w` -gt 0 ]; then
+    chkconfig crond on
+fi
 
 #####
 # Set the proper timezone.
@@ -37,7 +45,8 @@ elif [ -f /usr/share/zoneinfo/America/Denver ]; then
 fi
 
 #####
-# Enable Cron to run automatically.
+# Install ctest as a script.
+# It is installed into /etc/init.d/ctest
 #####
 if [ `which update-rc.d | wc -w` -gt 0 ]; then
     update-rc.d cron defaults
@@ -49,15 +58,19 @@ fi
 # Install CTest as a script in the 'vagrant' home directory.
 #####
 CTEST_INIT="/home/vagrant/ctest_service.sh"
+LOCKFILE="/vagrant/NOTEST"
 
 echo '#!/bin/bash' > $CTEST_INIT
+echo 'sleep 10' >> $CTEST_INIT
+echo "LFILE=\"$LOCKFILE\"" >> $CTEST_INIT
 echo 'CTEST_HOME="/home/vagrant/ctest_scripts"' >> $CTEST_INIT
 echo 'DASH="$CTEST_HOME/Dashboards"' >> $CTEST_INIT
+echo 'rm -rf $CTEST_HOME' >> $CTEST_INIT
 echo '/bin/mkdir -p $CTEST_HOME' >> $CTEST_INIT
 
 echo "count=0" >> $CTEST_INIT
 echo 'while [ $count -lt 10 ]; do' >> $CTEST_INIT
-echo " if [ -d /vagrant ]; then" >> $CTEST_INIT
+echo " if [ -f /vagrant/Vagrantfile ]; then" >> $CTEST_INIT
 echo "   count=50" >> $CTEST_INIT
 echo " else" >> $CTEST_INIT
 echo '   echo "Waiting for /vagrant to be mounted."' >> $CTEST_INIT
@@ -67,9 +80,9 @@ echo ' fi' >> $CTEST_INIT
 echo 'done' >> $CTEST_INIT
 
 echo "if [ -d /vagrant ]; then" >> $CTEST_INIT
-echo "find -L /vagrant -maxdepth 1 -type f -exec cp {} /home/vagrant/ctest_scripts \;" >> $CTEST_INIT
+echo "     find -L /vagrant -maxdepth 1 -type f -exec cp {} /home/vagrant/ctest_scripts \;" >> $CTEST_INIT
 echo "else" >> $CTEST_INIT
-echo "exit 1" >> $CTEST_INIT
+echo "     exit 1" >> $CTEST_INIT
 echo "fi" >> $CTEST_INIT
 
 #echo "chown -R vagrant:vagrant /home/vagrant/ctest_scripts" >> $CTEST_INIT 
@@ -79,7 +92,7 @@ echo '    echo "ctest not found"' >> $CTEST_INIT
 echo '    exit 1' >> $CTEST_INIT
 echo ' fi' >> $CTEST_INIT
 echo '      /bin/rm -rf $DASH' >> $CTEST_INIT
-echo '	    echo $"Starting ctest"' >> $CTEST_INIT
+echo '	    echo "Starting ctest"' >> $CTEST_INIT
 echo '      cd /home/vagrant/ctest_scripts/' >> $CTEST_INIT
 echo '      /usr/local/bin/ctest -V -S CI.cmake > continuous_test.out 2>&1 &' >> $CTEST_INIT
 
@@ -90,13 +103,15 @@ chmod 755 $CTEST_INIT
 
 
 ####
-# End installation of ctest as a system service.
+# End installation of ctest as a script.
 # Note: We can't start it yet, ctest and hdf libraries
 # haven't been installed yet.
 ####
 
 ### Install a crontab for running nightly tests.
 sudo -i -u vagrant echo "@reboot $CTEST_INIT" > /home/vagrant/crontab.in
+sudo -i -u vagrant echo '01 0 * * * cd /home/vagrant/ctest_scripts && /home/vagrant/ctest_scripts/run_nightly_test.sh > nightly_log.txt' >> /home/vagrant/crontab.in
+
 sudo -i -u vagrant crontab < /home/vagrant/crontab.in
 rm /home/vagrant/crontab.in
 
@@ -113,7 +128,13 @@ HDF5_VER="hdf5-1.8.13"
 # Install cmake from source
 if [ ! -f /usr/local/bin/cmake ]; then
     CMAKE_FILE="$CMAKE_VER".tar.gz
-    wget http://www.cmake.org/files/v2.8/$CMAKE_FILE
+    if [ ! -f "/vagrant/$CMAKE_FILE" ]; then
+	wget http://www.cmake.org/files/v2.8/$CMAKE_FILE
+	cp "$CMAKE_FILE" /vagrant
+    else
+	cp "/vagrant/$CMAKE_FILE" .
+    fi
+    
     tar -zxf $CMAKE_FILE
     pushd $CMAKE_VER
     ./configure --prefix=/usr/local
@@ -125,7 +146,13 @@ fi
 # Install hdf4 from source.
 if [ ! -f /usr/local/lib/libhdf4.settings ]; then
     HDF4_FILE="$HDF4_VER".tar.bz2
-    wget http://www.hdfgroup.org/ftp/HDF/HDF_Current/src/$HDF4_FILE
+    if [ ! -f "/vagrant/$HDF4_FILE" ]; then
+	wget http://www.hdfgroup.org/ftp/HDF/HDF_Current/src/$HDF4_FILE
+	cp "$HDF4_FILE" /vagrant
+    else
+	cp "/vagrant/$HDF4_FILE" .
+    fi
+
     tar -jxf $HDF4_FILE
     pushd $HDF4_VER
     ./configure --disable-static --enable-shared --disable-netcdf --disable-fortran --prefix=/usr/local
@@ -137,7 +164,13 @@ fi
 # Install hdf5 from source
 if [ ! -f /usr/local/lib/libhdf5.settings ]; then
     HDF5_FILE="$HDF5_VER".tar.bz2
-    wget http://www.hdfgroup.org/ftp/HDF5/current/src/$HDF5_FILE
+    if [ ! -f "/vagrant/$HDF5_FILE" ]; then
+	wget http://www.hdfgroup.org/ftp/HDF5/current/src/$HDF5_FILE
+	cp "$HDF5_FILE" /vagrant
+    else
+	cp "/vagrant/$HDF5_FILE" .
+    fi
+    
     tar -jxf $HDF5_FILE
     pushd $HDF5_VER
     ./configure --disable-static --enable-shared --disable-fortran --enable-hl --disable-fortran --prefix=/usr/local
